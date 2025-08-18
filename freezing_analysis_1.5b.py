9d897a7c8f341cf2df6139b4331cb48a6859aa46
duongxt <tungdx2006@gmail.com>
2025-08-18 11:32:07 -0700

    Update freezing_analysis_1.5b.py

--- freezing_analysis_1.5b.py
+++ freezing_analysis_1.5b.py
@@ -327,7 +327,7 @@ for folder_name in os.listdir(base_dir): # cycle throug every subfolder in the s
         times, brightness = zip(*droplet_brightness[droplet_id])
         times = np.array(times)
         brightness = np.array(brightness)
-        deriv = np.diff(brightness) * fps # convert brightness deriv to 1/s
+        deriv = np.diff(brightness) * fps * (1 + FRAME_SKIP) # convert brightness deriv to 1/s
         peaks, _ = find_peaks(deriv, prominence=BRIGHTNESS_PEAK_PROMINENCE) # returns frame num of deriv peaks
         
 
@@ -426,3 +426,4 @@ if all_results_by_folder:
     print(f"\nSaved: {excel_filename}")
 else:
     print("No nitric acid results found across all folders.")
+
